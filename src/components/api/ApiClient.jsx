3fc67112158b8b65b234b364fed814a388da7acd
chandragunda9 <chandragunda1215@gmail.com>
2024-12-15 00:38:06 +0530

    host change

--- src/components/api/ApiClient.jsx
+++ src/components/api/ApiClient.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const apiClient = axios.create(
     {
-        baseURL: 'http://localhost:5000'
+        // baseURL: 'http://localhost:5000'
+        baseURL: 'http://dsa-backend-application-env.eba-vr2ptug9.ap-south-1.elasticbeanstalk.com/'
     }
 )
